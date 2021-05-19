ff73c0182e4c44c3872268a932d8f849c82282c3
sjonathan85 <sjonchristensen@gmail.com>
2021-05-18 19:10:02 -0600

    alerts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert!  JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?")
+// Note the lack of quotation marks around playerName
+console.log(playerName)
+
+console.log("This logs a string, good for leaving yourself a message.");
+//this wil do math and log 20
+console.log(10 + 10);
+
+//what is this
+console.log("Our robot's name is " + playerName);
+
+// this creates the function called "fight"
+
+function fight() {
+    window.alert("The fight has begun.");
+}
+
+//fight();
+
